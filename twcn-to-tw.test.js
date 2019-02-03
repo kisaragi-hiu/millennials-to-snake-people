74ccec063184c43d687df65dfe195924d2fd3ce7
Kisaragi Hiu <mail@kisaragi-hiu.com>
2019-02-03 16:26:00 +0900

    split tests

--- twcn-to-tw.test.js
+++ twcn-to-tw.test.js
@@ -1,5 +1,13 @@
 let t = require("./twcn-to-tw.js");
 
-test("replace text", () => {
+test("-> Taiwan", () => {
     expect(t.replaceText("Taiwan")).toBe("Taiwan");
+    expect(t.replaceText("Taiwan, China")).toBe("Taiwan");
+    expect(t.replaceText("Taiwan, province of China")).toBe("Taiwan");
+});
+
+test("-> ROC", () => {
+    expect(t.replaceText("China Taiwan")).toBe("ROC (Taiwan)");
+    expect(t.replaceText("China, Taiwan")).toBe("ROC (Taiwan)");
+    expect(t.replaceText("China, Republic of")).toBe("Taiwan (ROC)");
 });
